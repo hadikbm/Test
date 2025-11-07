dcbccc6b81e353cba0b7f2c890ab50b81fb2b329
hadikbm <hfaour@smail.uni-koeln.de>
2025-11-07 16:45:54 +0100

    <init>

---
+++ src/h2/H2_main.java
@@ -0,0 +1,17 @@
+package h2;
+
+public class H2_main {
+
+	public static void main(String[] args) {
+	int jahr = 2025;
+	boolean schalt = true;
+
+	if ((jahr % 4 == 0 && jahr % 100 != 0)||(jahr % 400 == 0)) {
+		schalt = true;
+	} else {
+		schalt = false;
+	}
+	    System.out.println(schalt);
+	}
+
+}
